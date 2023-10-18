781c77fea6b04fa8ce6c0aa3d042701615cc3cb4
SwissPowershell <Supreme@sefanet.ch>
2023-10-18 17:54:53 +0200

    Update Debug.ps1

--- Debug.ps1
+++ Debug.ps1
@@ -29,47 +29,99 @@ $DebugStart = Get-Date
 # Test your functions here #
 ############################
 
-$PublicHolidays = @(
-    [System.DateOnly]::New(2023,1,1)    # Nouvel Ans
-    [System.DateOnly]::New(2023,4,7)    # Vendredi-Saint
-    [System.DateOnly]::New(2023,4,10)   # Lundi de Pâques
-    [System.DateOnly]::New(2023,5,18)   # Jeudi de l'Ascension
-    [System.DateOnly]::New(2023,5,29)   # Lundi de Pentecôte
-    [System.DateOnly]::New(2023,8,1)    # Fête Nationale
-    [System.DateOnly]::New(2023,9,7)    # Jeûne Genevois
-    [System.DateOnly]::New(2023,12,25)  # Noel
-    [System.DateOnly]::New(2023,12,31)  # Restauration de la République
+$PublicHolydays = @(
+    $([DateTime]::Parse('2023-01-01T00:00:00'))     # Nouvel Ans
+    $([DateTime]::Parse('2023-04-07T00:00:00'))     # Vendredi-Saint
+    $([DateTime]::Parse('2023-04-10T00:00:00'))     # Lundi de Paques
+    $([DateTime]::Parse('2023-05-18T00:00:00'))     # Jeudi de l'Ascension
+    $([DateTime]::Parse('2023-05-29T00:00:00'))     # Lundi de Pentecote
+    $([DateTime]::Parse('2023-08-01T00:00:00'))     # Fete Nationale
+    $([DateTime]::Parse('2023-09-07T00:00:00'))     # Jeune Genevois
+    $([DateTime]::Parse('2023-12-25T00:00:00'))     # Noel
+    $([DateTime]::Parse('2023-12-31T00:00:00'))     # Restauration de la Republique
 )
+# add the week end to my PublicHolidays as it is in our database otherwyse you can set $OOORuleSet.WeekEndInHolidays = $False and it will take into account the given working days
+$Start = [DateTime]::Parse('2023-01-01T00:00:00')
+$CurrDate = $Start
+Do {
+    if (($CurrDate.DayOfWeek -eq [DayOfWeek]::Saturday) -or ($CurrDate.DayOfWeek -eq [DayOfWeek]::Sunday)) {
+        if ($CurrDate -NotIn $PublicHolydays) {
+            $PublicHolydays += $CurrDate
+        }
+    }
+    $CurrDate = $CurrDate.AddDays(1)
+}Until($CurrDate.Year -eq 2024)
 $VerbosePreference = 'Continue'
-$OOORuleSet = New-OOORuleSet -PublicHolidays $PublicHolidays
-#$OOORuleSet.PublicHolidays = $PublicHolidays
-$NewYear = Get-Date -Year 2023 -Month 1 -Day 1
-$RequestStart1 = Get-Date -Year 2023 -Month 5 -Day 30 -Hour 8 -Minute 0 -Second 0 -Millisecond 0
+$O3RuleSet = New-O3RuleSet
+$O3RuleSet.PublicHolydays = $PublicHolydays
+
+$PersonalHolyDays = New-O3List -O3RuleSet $O3RuleSet
+
+$RequestStart1 = Get-Date -Year 2023 -Month 5 -Day 30 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
 $RequestStop1 = Get-Date -Year 2023 -Month 6 -Day 2 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
-$PersonalHolydayRequest1 = New-PersonalHolydayRequest -Start $RequestStart1 -Stop $RequestStop1 -OOORuleSet $OOORuleSet # take the week of "Lundi de pentecote"
-$PersonalHolydayRequest2 = New-PersonalHolydayRequest -Start $RequestStart1 -Stop $RequestStop1 -OOORuleSet $OOORuleSet # take the week of "Lundi de pentecote" a second time (should do nothing)
+$PersonalHolydayRequest1 = New-O3Request -Start $RequestStart1 -Stop $RequestStop1 -O3RuleSet $O3RuleSet # take the week of "Lundi de pentecote"
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest1)
 
-$RequestStart3 = Get-Date -Year 2023 -Month 7 -Day 31 -Hour 8 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest2 = New-O3Request -Start $RequestStart1 -Stop $RequestStop1 -O3RuleSet $O3RuleSet # take the week of "Lundi de pentecote" a second time (should do nothing)
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest2)
+
+
+$RequestStart3 = Get-Date -Year 2023 -Month 7 -Day 31 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
 $RequestStop3 = Get-Date -Year 2023 -Month 8 -Day 2 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
-$PersonalHolydayRequest3 = New-PersonalHolydayRequest -Start $RequestStart3 -Stop $RequestStop3 -OOORuleSet $OOORuleSet # take the monday before "Fête Nationale" and the day after (Wednesday)
+$PersonalHolydayRequest3 = New-O3Request -Start $RequestStart3 -Stop $RequestStop3 -O3RuleSet $O3RuleSet # take the monday before "Fete Nationale" and the day after (Wednesday)
 
-$RequestStop4 = Get-Date -Year 2023 -Month 8 -Day 3 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
-$PersonalHolydayRequest4 = New-PersonalHolydayRequest -Start $RequestStart3 -Stop $RequestStop4 -OOORuleSet $OOORuleSet # take the monday before "Fête Nationale" and 2 days after (Thursday)
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest3)
+
+$RequestStop4 = Get-Date -Year 2023 -Month 8 -Day 3 -Hour 12 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest4 = New-O3Request -Start $RequestStart3 -Stop $RequestStop4 -O3RuleSet $O3RuleSet # take the monday before "Fete Nationale" and 1.5 days after (Thursday)
+
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest4)
 
 $RequestStop5 = Get-Date -Year 2023 -Month 8 -Day 4 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
-$PersonalHolydayRequest5 = New-PersonalHolydayRequest -Start $RequestStop3 -Stop $RequestStop5 -OOORuleSet $OOORuleSet # take the day after "Fête Nationale" and 3 days after (Friday)
-
-$PersonalHolyDays = New-PersonalHolydaysList -OOORuleSet $OOORuleSet
-$PersonalHolyDays.AddRequest($PersonalHolydayRequest1)
-$PersonalHolyDays.AddRequest($PersonalHolydayRequest2)
-$PersonalHolyDays.AddRequest($PersonalHolydayRequest3)
-$PersonalHolyDays.AddRequest($PersonalHolydayRequest4)
+$PersonalHolydayRequest5 = New-O3Request -Start $RequestStop3 -Stop $RequestStop5 -O3RuleSet $O3RuleSet # take the day after "Fete Nationale" and 3 days after (Friday)
 $PersonalHolyDays.AddRequest($PersonalHolydayRequest5)
 
-$RequestStart6 = Get-Date -Year 2023 -Month 8 -Day 2 -Hour 8 -Minute 0 -Second 0 -Millisecond 0
-$RequestStop6 = Get-Date -Year 2023 -Month 8 -Day 3 -Hour 8 -Minute 0 -Second 0 -Millisecond 0
-$PersonalHolydayRequest6 = New-PersonalHolydayRequest -Start $RequestStart6 -Stop $RequestStop6 -OOORuleSet $OOORuleSet
-$PersonalHolyDays.RemoveRequest($PersonalHolydayRequest6) # Remove 2 and 3 of august
+
+$RequestStart6 = Get-Date -Year 2023 -Month 8 -Day 2 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
+$RequestStop6 = Get-Date -Year 2023 -Month 8 -Day 2 -Hour 12 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest6 = New-O3Request -Start $RequestStart6 -Stop $RequestStop6 -O3RuleSet $O3RuleSet -Remove
+$PersonalHolyDays.RemoveRequest($PersonalHolydayRequest6) # Remove 2 in the morning shoud do a 2+
+
+$RequestStart7 = Get-Date -Year 2023 -Month 8 -Day 3 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
+$RequestStop7 = Get-Date -Year 2023 -Month 8 -Day 4 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest7 = New-O3Request -Start $RequestStart7 -Stop $RequestStop7 -O3RuleSet $O3RuleSet -Remove
+$PersonalHolyDays.RemoveRequest($PersonalHolydayRequest7) # Remove 3 and 4 of august
+
+$RequestStart8 = Get-Date -Year 2023 -Month 8 -Day 3 -Hour 14 -Minute 0 -Second 0 -Millisecond 0
+$RequestStop8 = Get-Date -Year 2023 -Month 8 -Day 4 -Hour 12 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest8 = New-O3Request -Start $RequestStart8 -Stop $RequestStop8 -O3RuleSet $O3RuleSet
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest8) # Take from 3 14h to 4 12h => 3+ 4-
+
+# TO DO :
+#   Check what happen if I take 1 week off 1 week in then 1 week off and then I take the week in => off
+#   4.9 -> 8.9
+#   18.9 -> 22.9
+#   11.9 -> 15.9
+#   It normally should become 1 unique off from 2 to 24 (but it will be two) => This has to be resolved
+
+$RequestStart9 = Get-Date -Year 2023 -Month 9 -Day 4 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
+$RequestStop9 = Get-Date -Year 2023 -Month 9 -Day 8 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest9 = New-O3Request -Start $RequestStart9 -Stop $RequestStop9 -O3RuleSet $O3RuleSet
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest9) #   4.9 -> 8.9
+
+$RequestStart10 = Get-Date -Year 2023 -Month 9 -Day 18 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
+$RequestStop10 = Get-Date -Year 2023 -Month 9 -Day 22 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest10 = New-O3Request -Start $RequestStart10 -Stop $RequestStop10 -O3RuleSet $O3RuleSet
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest10) #   18.9 -> 22.9
+
+$RequestStart11 = Get-Date -Year 2023 -Month 9 -Day 11 -Hour 7 -Minute 0 -Second 0 -Millisecond 0
+$RequestStop11 = Get-Date -Year 2023 -Month 9 -Day 15 -Hour 18 -Minute 0 -Second 0 -Millisecond 0
+$PersonalHolydayRequest11 = New-O3Request -Start $RequestStart11 -Stop $RequestStop11 -O3RuleSet $O3RuleSet
+$PersonalHolyDays.AddRequest($PersonalHolydayRequest11) #   11.9 -> 15.9
+
+$VerbosePreference = 'SilentlyContinue'
+Write-O3Calendar -StartYear 2023 -PersonalHolyDays $PersonalHolyDays -OOORuleSet $O3RuleSet
+
 ##################################
 # End of the tests show mettrics #
 ##################################
